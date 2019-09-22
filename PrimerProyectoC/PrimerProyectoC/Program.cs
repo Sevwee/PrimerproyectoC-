2a5e3ffcae1f99e5930014d451406afb91d07df1
Antonio Morón Pozo <sevillista-97@hotmail.com>
2019-09-22 19:54:38 +0200

    "Primer" Proytecto subido a Git

---
+++ PrimerProyectoC/PrimerProyectoC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un valor para el primer String");
+            String a = Console.ReadLine();
+            Console.WriteLine("Introduce un valor para el primer String");
+            String b = Console.ReadLine();
+
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine(a + b);
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
